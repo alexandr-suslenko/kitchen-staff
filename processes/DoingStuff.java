327253287099bd76b3dd39339a366322d4fd7bca
Suslenko Alexandr <alexandr.suslenko@gmail.com>
2013-11-04 11:07:03 +0200

    Добавление класса DoingStaff, запускающего процесс кипячения воды BoilingWater.

---
+++ processes/DoingStuff.java
@@ -0,0 +1,12 @@
+package processes;
+
+/**
+ * @author Suslenko Alexandr
+ */
+public class DoingStuff {
+
+	public static void main(String[] args) {
+		BoilingWater p = new BoilingWater(0.1f, 85, 1);
+		p.boilWater();
+	}
+}
